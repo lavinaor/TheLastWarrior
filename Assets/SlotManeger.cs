89a3507dd5f790f2b5c2509438e71a11345beff5
lavi <lavinaorab@gmail.com>
2024-09-08 15:07:17 +0300

    slot works

--- Assets/SlotManeger.cs
+++ Assets/SlotManeger.cs
@@ -37,27 +37,24 @@ public class SlotManeger : MonoBehaviour
     }
     public void ExecuteSlotAction()
     {
-        if (playerCombatController.isAttackung == false)
+        Debug.Log("enterd " + Name);
+        if (AttackInCooldown <= 0f && playerCombatController.isAttackung == false)
         {
-            Debug.Log("enterd " + Name);
-            if (AttackInCooldown <= 0f)
+            Debug.Log("enterd 1 " + Name);
+            action.ExecuteAction();
+            AttackInCooldown = AttackTime;
+            CooldownImageFill.color = new Color(0.75f, 0.0f, 1.0f, 1.0f);
+            UpdateCooldownUI();
+            isActive = true;
+            Invoke("StartCooldown", AttackTime);
+        }
+        else
+        {
+            if (canEnterWhileAttack && isActive)
             {
-                Debug.Log("enterd 1 " + Name);
+                isActive = false;
+                Debug.Log("enterd 2 " + Name);
                 action.ExecuteAction();
-                AttackInCooldown = AttackTime;
-                CooldownImageFill.color = new Color(0.75f, 0.0f, 1.0f, 1.0f);
-                UpdateCooldownUI();
-                isActive = true;
-                Invoke("StartCooldown", AttackTime);
-            }
-            else
-            {
-                if (canEnterWhileAttack && isActive)
-                {
-                    isActive = false;
-                    Debug.Log("enterd 2 " + Name);
-                    action.ExecuteAction();
-                }
             }
         }
     }
